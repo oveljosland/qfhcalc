a0c9c6c99e8c1c8c496200ba0ba624b730262e68
oveljosland <github.com.hazy109@passmail.net>
2025-10-14 19:10:17 +0200

    add delta l routine

--- qfhcalc.c
+++ qfhcalc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define MAXINTV 15
+
 double dl(double);
 double df(double);
 void calc(double f, double wd, double wr, double r, double t, double nw);
@@ -31,6 +33,22 @@ main()
 	calc(f, wd, wr, r, t, nw);
 	return 0;
 }
+
+double dl(double diam)
+{
+	double tab[] = {
+		1.045, 1.053, 1.060, 1.064, 1.068, 1.070, 1.070, 1.071,
+		1.071, 1.070, 1.070, 1.070, 1.070, 1.069, 1.069, 1.068, 1.067
+	};
+
+	int i = (int)diam;
+	if (i < 0)
+		i = 0;
+	if (i >= MAXINTV+1)
+		i = MAXINTV;
+	return tab[i] + (tab[i+1] - tab[i]) * (diam - i);
+}
+
 
 
 	
